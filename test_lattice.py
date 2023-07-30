2c30b9e9e0cc00c5bb1a24fc6c28e860ff7bfa8e
Simon <simonjwoodward5@gmail.com>
2023-07-31 00:24:39 +0100

    Added non-zero lattice load test
    Added a lattice save/load test that used values that weren't zero for the lattice size

--- test_lattice.py
+++ test_lattice.py
@@ -1,3 +1,4 @@
+import numpy as np
 import unittest
 import os
 from lattice import Lattice
@@ -7,6 +8,16 @@ class MyTestCase(unittest.TestCase):
 
     def test_blank_save_load(self):
         default_lattice = Lattice()
+        default_lattice.save_to_file("tmp")
+        loaded_lattice = Lattice("tmp")
+        os.remove("tmp.json")
+        self.assertEqual(default_lattice, loaded_lattice)
+
+    def test_positive_save_load(self):
+        default_lattice = Lattice()
+        default_lattice.number_of_time_points = 10
+        default_lattice.number_of_spacial_points = np.array([1, 0, 0])
+
         default_lattice.save_to_file("tmp")
         loaded_lattice = Lattice("tmp")
         os.remove("tmp.json")
